84d5b8d7fc6fd2ccbdacc2cacc9174edd7210da9
nohsangwoo <nsgr12@naver.com>
2021-03-18 17:44:26 +0900

    apply devtools and loger &

--- src/middlewares/myLogger.js
+++ src/middlewares/myLogger.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const myLogger = store => next => action => {
   console.log(action);
+  if (action.type === 'INCREASE') {
+    console.log('특정조건');
+  }
+
+  console.log('/t', store.getState());
+
   const result = next(action);
+
   console.log('/t', store.getState());
 
   return result;
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 import myLogger from '../middlewares/myLogger';
 import rootReducer from '../modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export default createStore(rootReducer, applyMiddleware(myLogger));
+// export default createStore(rootReducer, applyMiddleware(myLogger, logger));
+export default createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
