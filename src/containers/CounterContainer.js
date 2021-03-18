1dfc7231453f2de83fc32f23f564a7a3ae7f9217
nohsangwoo <nsgr12@naver.com>
2021-03-18 18:26:33 +0900

    redux thunk

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
-import { decrease, increase } from '../modules/counter';
+import {
+  decrease,
+  decreaseAsync,
+  increase,
+  increaseAsync,
+} from '../modules/counter';
 
 function CounterContainer() {
   const number = useSelector(state => state.counter);
   const dispatch = useDispatch();
 
   const onIncrease = () => {
-    dispatch(increase());
+    dispatch(increaseAsync());
   };
   const onDecrease = () => {
-    dispatch(decrease());
+    dispatch(decreaseAsync());
   };
   return (
     <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,9 @@ import logger from 'redux-logger';
 import myLogger from '../middlewares/myLogger';
 import rootReducer from '../modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import ReduxThunk from 'redux-thunk';
 // export default createStore(rootReducer, applyMiddleware(myLogger, logger));
 export default createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
 );
